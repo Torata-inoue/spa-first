e4801ae62d593e91d5e02b129aaddbb4c50f0003
Torata-Inoue <i_am_torao@yahoo.co.jp>
2021-08-04 09:15:48 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,58 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios';
 
-function App() {
+const App = function() {
+  const res = axios.get('http://localhost:3001/comments/2');
+
+  // axios.post('http://localhost:3001/tasks', {body: '追加要素'}, {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   }
+  // });
+
+  // axios.put('http://localhost:3001/tasks/3', {body: '変更した要素'}, {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   }
+  // });
+
+  // axios.delete('http://localhost:3001/tasks/3', {
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   }
+  // });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+    <div>
+      aaaaaaaa
     </div>
-  );
+  )
+
 }
 
 export default App;
